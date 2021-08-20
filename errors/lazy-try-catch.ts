4dcac3f1247ba5f913087f9ccba10b75f1ca1772
trapcodeio <trapcode.io@gmail.com>
2021-08-20 19:08:21 +0100

    First Serious Commit

--- errors/lazy-try-catch.ts
+++ errors/lazy-try-catch.ts
@@ -19,11 +19,11 @@ export function tryCatch<T, E = Error>(fn: () => T): [undefined, T] | [E, undefi
  * Returns and array of error and data.
  *
  * error will be undefined if there is no error.
- * @param fn
+ * @param promise
  */
-export async function resolve<T, E = Error>(fn: () => Promise<T>): Promise<[undefined, T] | [E, undefined]> {
+export async function resolve<T, E = Error>(promise: Promise<T>): Promise<[undefined, T] | [E, undefined]> {
     try {
-        return [undefined, await fn()]
+        return [undefined, await promise]
     } catch (e) {
         return [e, undefined]
     }
